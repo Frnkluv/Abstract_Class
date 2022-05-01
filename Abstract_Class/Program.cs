a865ce70f0d2c8af3ca2eff5f967ad77f3290725
Egor <Egor@ASUS-EGORM>
2022-05-02 00:14:48 +0300

    Добавьте файлы проекта.

---
+++ Abstract_Class/Program.cs
@@ -0,0 +1,66 @@
+﻿Player soilder = new Player();
+
+Weapon[] inventory = { new Pistol(), new AR(), new LaserGun() };
+
+foreach (var item in inventory)
+{
+    soilder.CheckInfo(item);
+    soilder.Fire(item);
+    Console.WriteLine();
+}
+
+class Player
+{
+    public void Fire(Weapon weapon)
+    {
+        weapon.Fire();
+    }
+
+    public void CheckInfo(Weapon weapon)
+    {
+        weapon.ShowInfo();
+    }
+}
+
+
+abstract class Weapon
+{
+    public abstract int Damage { get; }
+
+    public abstract void Fire();
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"{GetType().Name} | Damage: {Damage}");
+    }
+}
+
+class Pistol : Weapon
+{
+    public override int Damage { get { return 10; } }
+
+    public override void Fire()
+    {
+        Console.WriteLine("Пиу, пиу");
+    }
+}
+
+class LaserGun : Weapon
+{
+    public override int Damage => 12;       /* Это лямбда выражение. Функциональность как у свойства выше в Pistol */
+
+    public override void Fire()
+    {
+        Console.WriteLine("ХитCкан");
+    }
+}
+
+class AR : Weapon
+{
+    public override int Damage => 15;
+
+    public override void Fire()
+    {
+        Console.WriteLine("Тррррр");
+    }
+}
